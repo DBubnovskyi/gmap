fb399327baa395583a5534042188a8c1e0aba1ab
DBubnovskyi <dima.bubnovskyi@gmail.com>
2022-06-17 22:02:14 +0300

    -

--- MapForms/Controls/MapControl.cs
+++ MapForms/Controls/MapControl.cs
@@ -52,6 +52,7 @@ namespace MapForms.Controls
 
         GMapOverlay routes = new GMapOverlay("gMapControl");
         GMapOverlay markers = new GMapOverlay("markers");
+        GMapOverlay markers1 = new GMapOverlay("markers1");
         GMapOverlay polyOverlay = new GMapOverlay("polygons");
         List<PointLatLng> points = new List<PointLatLng>();
         List<PointLatLng> list = new List<PointLatLng>();
@@ -111,12 +112,13 @@ namespace MapForms.Controls
                     marker.ToolTip.Stroke.Color = Color.FromArgb(0, 255, 255, 0);
                     marker.ToolTip.Font = new Font("Arial", 8, FontStyle.Regular);
                     marker.ToolTip.Fill = new SolidBrush(Color.Transparent);
-                    marker.ToolTip.Offset = new Point(4,-4);
+                    marker.ToolTip.Foreground = new SolidBrush(Color.Red);
+                    marker.ToolTip.Offset = new Point(8,-8);
                     marker.Offset = new Point(-12, -12);
                     markers.Markers.Add(marker);
                     gMapControl.Overlays.Add(markers);
 
-                    CreateCircle(coordinates, 10);
+                    CreateCircle2(coordinates, 10);
                 }
             }
         }
@@ -154,5 +156,80 @@ namespace MapForms.Controls
             markers.Polygons.Add(gpol);
             gMapControl.Overlays.Add(polyOverlay);
         }
+
+
+        private void CreateCircle2(PointLatLng point, double radius, int ColorIndex = 1)
+        {
+            int startAngle = 90;
+            int endAngle = 360;
+
+            List<PointLatLng> gpollist = new List<PointLatLng>();
+
+            if ((startAngle != 0 && endAngle != 360) || (startAngle != 360 && endAngle != 0))
+            {
+                gpollist.Add(point);
+            }
+            for (; startAngle < endAngle; startAngle++)
+            {
+                var target = FindPointAtDistanceFrom(point, startAngle * (Math.PI / 180), radius);
+                gpollist.Add(target);
+            }
+
+            GMapPolygon polygon = new GMapPolygon(gpollist, "Circle");
+            switch (ColorIndex)
+            {
+                case 1:
+                    polygon.Fill = new SolidBrush(Color.FromArgb(80, Color.Red));
+                    break;
+                case 2:
+                    polygon.Fill = new SolidBrush(Color.FromArgb(80, Color.Orange));
+                    break;
+                case 3:
+                    polygon.Fill = new SolidBrush(Color.FromArgb(20, Color.Aqua));
+                    break;
+                default:
+                    MessageBox.Show("No search zone found!");
+                    break;
+            }
+
+            polygon.Stroke = new Pen(Color.Red, 1);
+            gMapControl.Overlays.Remove(markers1);
+            markers1.Polygons.Add(polygon);
+            gMapControl.Overlays.Add(markers1);
+        }
+
+        public static PointLatLng FindPointAtDistanceFrom(PointLatLng startPoint, double initialBearingRadians, double distanceKilometres)
+        {
+            const double radiusEarthKilometres = 6371.01;
+            var distRatio = distanceKilometres / radiusEarthKilometres;
+            var distRatioSine = Math.Sin(distRatio);
+            var distRatioCosine = Math.Cos(distRatio);
+
+            var startLatRad = DegreesToRadians(startPoint.Lat);
+            var startLonRad = DegreesToRadians(startPoint.Lng);
+
+            var startLatCos = Math.Cos(startLatRad);
+            var startLatSin = Math.Sin(startLatRad);
+
+            var endLatRads = Math.Asin((startLatSin * distRatioCosine) + (startLatCos * distRatioSine * Math.Cos(initialBearingRadians)));
+            var endLonRads = startLonRad + Math.Atan2(Math.Sin(initialBearingRadians) * distRatioSine * startLatCos, distRatioCosine - startLatSin * Math.Sin(endLatRads));
+
+            return new PointLatLng(RadiansToDegrees(endLatRads), RadiansToDegrees(endLonRads));
+        }
+
+        public static double DegreesToRadians(double degrees)
+        {
+            const double degToRadFactor = Math.PI / 180;
+            return degrees * degToRadFactor;
+        }
+
+        public static double RadiansToDegrees(double radians)
+        {
+            const double radToDegFactor = 180 / Math.PI;
+            return radians * radToDegFactor;
+        }
+
+
+
     }
 }
