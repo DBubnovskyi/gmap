0db6439a0d81ee1553166455443b2588654f8ee3
DBubnovskyi <dima.bubnovskyi@gmail.com>
2022-06-20 13:20:43 +0300

    update

--- MapWPF/MainWindow.xaml.cs
+++ MapWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MapWPF.Controls.Buttons;
 
 namespace MapWPF
 {
