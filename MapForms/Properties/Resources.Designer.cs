2e1491934221e554b1fa18ee6b4a62531e31dffc
DESKTOP-QS0B0VT\ADSAD <dima.bubnovskyi@gmail.com>
2022-06-29 21:50:23 +0300

    --

--- MapForms/Properties/Resources.Designer.cs
+++ MapForms/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace MapForms.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap cruise_missaile {
+            get {
+                object obj = ResourceManager.GetObject("cruise missaile", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap dot_blue {
             get {
                 object obj = ResourceManager.GetObject("dot_blue", resourceCulture);
