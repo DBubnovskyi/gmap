93764cf86b7d5ff604cb8fcdf1e5ab93f12a6cf8
DBubnovskyi <dima.bubnovskyi@gmail.com>
2022-07-28 19:31:12 +0300

    --

--- MapProcassor/Program.cs
+++ MapProcassor/Program.cs
@@ -26,71 +26,71 @@ namespace MapProcassor
 
         static void Main(string[] args)
         {
-            //var data = new CityData().GetCities();
+            var data = new CityData().GetCities();
 
-            //var admin = data.FindAll(x => x.Capital == "admin"
-            //    || x.Capital == "primary");
-            //    //|| x.Capital == "minor");
+            var admin = data.FindAll(x => x.Capital == "admin"
+                || x.Capital == "primary");
+            //|| x.Capital == "minor");
 
-            //foreach (var region in admin)
-            //{
-            //    string reginName = region.CityName;
-
-            //    string url = $"https://nominatim.openstreetmap.org/search.php?q={reginName} Ukraine&format=jsonv2";
-            //    string json = Loader.LoadJson(url).UTF8();
-            //    var nodes = Geonode.FromJson(json);
-            //    if(nodes.Count == 0)
-            //    {
-            //        continue;
-            //    }
-            //    var geonode = nodes.Find(x => x.Type == "city") ??
-            //        nodes.Find(x => x.PlaceRank == 18) ?? nodes[0];
-            //    geonode.DisplayName = geonode.DisplayName.Split(',')[0];
-
-            //    Console.Write(geonode.DisplayName + "\n".UTF8());
-
-            //    if (region.Lat != 0 && region.Lon != 0)
-            //    {
-            //        geonode.Lat = region.Lat;
-            //        geonode.Lon = region.Lon;
-            //    }
-
-            //    //foreach (var node in nodes)
-            //    //{
-            //    //    Console.WriteLine($"\t\t\t" +
-            //    //        $"{node.PlaceRank} " +
-            //    //        $"{node.OsmId} " +
-            //    //        $"{node.OsmType} " +
-            //    //        $"{node.Type} " +
-            //    //        $"{node.Category} " +
-            //    //        $"{node.Importance} " +
-            //    //        $"{node.DisplayName}");
-            //    //}
-
-            //    string dataString = geonode.ToMapDataString();
-
-            //    using (StreamWriter writer = new StreamWriter($"{AssemblyDirectory}/{geonode.DisplayName}.json"))
-            //    {
-            //        writer.WriteLine(dataString);
-            //    }
-            //    System.Threading.Thread.Sleep(100);
-            //}
-
-
-            var alerts = new Alerts().Convert();
-            foreach (var alert in alerts)
+            foreach (var region in admin)
             {
-                string reginName = alert.Name;
-                string url = $"https://nominatim.openstreetmap.org/search.php?q={reginName}&format=jsonv2";
-                string json = Loader.LoadJson(url);
-                var region = Geonode.FromJson(json);
-                long osmId = region[0].OsmId;
+                string reginName = region.CityName;
 
-                url = $"https://polygons.openstreetmap.fr/get_geojson.py?id={region[0].OsmId}";
-                json = Loader.LoadJson(url);
-                region = Geonode.FromJson(json);
-                Console.WriteLine(json);
+                string url = $"https://nominatim.openstreetmap.org/search.php?q={reginName} Ukraine&format=jsonv2";
+                string json = Loader.LoadJson(url).UTF8();
+                var nodes = Geonode.FromJson(json);
+                if (nodes.Count == 0)
+                {
+                    continue;
+                }
+                var geonode = nodes.Find(x => x.Type == "city") ??
+                    nodes.Find(x => x.PlaceRank == 18) ?? nodes[0];
+                geonode.DisplayName = geonode.DisplayName.Split(',')[0];
+
+                Console.Write(geonode.DisplayName + "\n".UTF8());
+
+                if (region.Lat != 0 && region.Lon != 0)
+                {
+                    geonode.Lat = region.Lat;
+                    geonode.Lon = region.Lon;
+                }
+
+                //foreach (var node in nodes)
+                //{
+                //    Console.WriteLine($"\t\t\t" +
+                //        $"{node.PlaceRank} " +
+                //        $"{node.OsmId} " +
+                //        $"{node.OsmType} " +
+                //        $"{node.Type} " +
+                //        $"{node.Category} " +
+                //        $"{node.Importance} " +
+                //        $"{node.DisplayName}");
+                //}
+
+                string dataString = geonode.ToMapDataString();
+
+                using (StreamWriter writer = new StreamWriter($"{AssemblyDirectory}/{geonode.DisplayName}.json"))
+                {
+                    writer.WriteLine(dataString);
+                }
+                System.Threading.Thread.Sleep(100);
             }
+
+
+            //var alerts = new Alerts().Convert();
+            //foreach (var alert in alerts)
+            //{
+            //    string reginName = alert.Name;
+            //    string url = $"https://nominatim.openstreetmap.org/search.php?q={reginName}&format=jsonv2";
+            //    string json = Loader.LoadJson(url);
+            //    var region = Geonode.FromJson(json);
+            //    long osmId = region[0].OsmId;
+
+            //    url = $"https://polygons.openstreetmap.fr/get_geojson.py?id={region[0].OsmId}";
+            //    json = Loader.LoadJson(url);
+            //    region = Geonode.FromJson(json);
+            //    Console.WriteLine(json);
+            //}
 
             Console.WriteLine("\nfinished");
             //while (Console.ReadKey().Key != ConsoleKey.Escape);
